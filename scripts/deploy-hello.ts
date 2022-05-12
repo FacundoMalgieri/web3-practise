9e9107a703f8507a08c009d3a1fecb3bfb33c1a1
FacundoMalgieri <facundo.malgieri@gmail.com>
2022-05-12 16:54:55 -0300

    Init.

---
+++ scripts/deploy-hello.ts
@@ -0,0 +1,1 @@
+import '@nomiclabs/hardhat-ethers';import { ethers } from 'hardhat';export async function deployHello() {    const HelloWorld = await ethers.getContractFactory('HelloWorld');    const hello = await HelloWorld.deploy();    await hello.deployed();    return hello;}async function sayHello(hello: any) {    console.log(await hello.hello());}deployHello().then(sayHello)
---
+++ test/HelloWorld.spec.ts
@@ -0,0 +1,1 @@
+import '@nomiclabs/hardhat-ethers';import { expect } from 'chai';import { deployHello } from '../scripts/deploy-hello';describe('hello world', () => {    it('should say hi', async () => {        const hello = await deployHello()        expect(await hello.hello()).to.equal("Hello, World");    });});
